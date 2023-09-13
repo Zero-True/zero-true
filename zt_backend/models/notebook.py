49a4cd0ee74713b16034efdc00b57b0e9e28596d
Carson-Shaar <carson.shaar@zero-true.com>
2023-09-13 18:06:23 -0400

    Notebook deserialization fix

--- zt_backend/models/notebook.py
+++ zt_backend/models/notebook.py
@@ -1,6 +1,17 @@
-from pydantic import BaseModel, Field, SerializeAsAny
-from typing import OrderedDict, List
+from pydantic import BaseModel, Field, SerializeAsAny, root_validator
+from typing import OrderedDict, List, Dict, Any
 from zt_backend.models.components.zt_component import ZTComponent
+from zt_backend.models.components.slider import Slider
+
+def deserialize_component(data: Dict[str, Any]) -> ZTComponent:
+    component_map = {
+        "v-slider": Slider,
+        # add other component types here
+    }
+    component_class = data.get("component")
+    if component_class not in component_map:
+        raise ValueError(f"Invalid component class: {component_class}")
+    return component_map[component_class].model_validate(data)
 
 class CodeCell(BaseModel):
     id: str
@@ -8,6 +19,12 @@ class CodeCell(BaseModel):
     output: str
     components: List[SerializeAsAny[ZTComponent]]
     cellType: str = Field('code', enum=['code', 'markdown'])
+
+    @root_validator(pre=True)
+    def deserialize_components(cls, values):
+        components = values.get('components', [])
+        values['components'] = [deserialize_component(comp) for comp in components]
+        return values
     
 class Notebook(BaseModel):
     cells: OrderedDict[str, CodeCell]
