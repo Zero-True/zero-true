689b1d4b43b04cbe5185e71b4e860fa5279476f1
Carson-Shaar <carson.shaar@zero-true.com>
2023-09-13 15:59:49 -0400

    Load and maintain project toml

--- zt_backend/router.py
+++ zt_backend/router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
 from fastapi.responses import FileResponse
-from zt_backend.models import request
-from zt_backend.models import notebook
+from zt_backend.models import request, notebook, response
 from runner.execute_code import execute_request
 import os
 import uuid
+import toml
 
 router = APIRouter()
 
@@ -20,15 +20,43 @@ def health():
     return('UP')
 
 @router.post("/api/runcode")
-async def runcode(request: request.Request):
-    return execute_request(request)
+def runcode(request: request.Request):
+    globalStateUpdate(run_request=request)
+    response = execute_request(request)
+    globalStateUpdate(run_response=response)
+    return response
 
 @router.post("/api/create_cell")
 def create_cell():
-     return notebook.CodeCell(
+     createdCell = notebook.CodeCell(
          id=str(uuid.uuid4()),
          code='#code here or else',
          components=[],
          output='',
          cellType='code'
      )
+     globalStateUpdate(newCell=createdCell)
+     return createdCell
+
+@router.get("/api/notebook")
+def get_notebook():
+    return get_notebook()
+
+def get_notebook():
+    with open('notebook.toml', "r") as project_file:
+        notebook_data = toml.load(project_file)
+    return notebook.Notebook(**notebook_data)
+
+def globalStateUpdate(newCell: notebook.CodeCell=None, run_request: request.Request=None, run_response: response.Response=None):
+    zt_notebook = get_notebook()
+    if newCell is not None:
+        zt_notebook.cells[newCell.id] = newCell
+    if run_request is not None:
+        for requestCell in run_request.cells:
+            zt_notebook.cells[requestCell.id].code = requestCell.code
+    if run_response is not None:
+        for responseCell in run_response.cells:
+            zt_notebook.cells[responseCell.id].components = responseCell.components
+            zt_notebook.cells[responseCell.id].output = responseCell.output
+    with open('notebook.toml', "w") as project_file:
+        toml.dump(zt_notebook.model_dump(), project_file)
