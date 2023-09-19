81746484ae97dd970e1009e96eb9a5aafefef4e0
Carson-Shaar <carson.shaar@zero-true.com>
2023-09-19 16:47:24 -0400

    GHA for publishing to PyPI

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# Read the contents of requirements.txt
-with open('zt_backend/requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name='zero-true',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=requirements,  # Use the requirements read from the file
-    entry_points={
-        'console_scripts': [
-            'zero-true=zt_cli.cli:start_servers',
-            'zero-true-dev=zt_dev_cli.cli:zt_cli',
-        ],
-        
-    }
-)
+setup()
