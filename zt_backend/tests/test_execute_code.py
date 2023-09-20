0e3e49428daba11816d96f0594ec4d94e8e2a3c6
Carson-Shaar <carson.shaar@zero-true.com>
2023-09-20 18:25:01 -0400

    tests

--- zt_backend/tests/test_execute_code.py
+++ zt_backend/tests/test_execute_code.py
@@ -8,7 +8,7 @@ def setup_function():
 # Test for simple variable assignment
 def test_execute_request_simple_variable():
     setup_function()
-    req = Request(originId="0", cells=[CodeRequest(id="0", code="a = 1")], components={})
+    req = Request(originId="0", cells=[CodeRequest(id="0", code="a = 1", cellType='code')], components={})
     execute_request(req)
     assert 'a' in cell_outputs_dict['0']
     assert cell_outputs_dict['0']['a'] == 1
@@ -16,22 +16,22 @@ def test_execute_request_simple_variable():
 # Test for function definition
 def test_execute_request_function_definition():
     setup_function()
-    req = Request(originId="1", cells=[CodeRequest(id="1", code="def add(x, y): return x + y")], components={})
+    req = Request(originId="1", cells=[CodeRequest(id="1", code="def add(x, y): return x + y", cellType='code')], components={})
     execute_request(req)
     assert 'add' in cell_outputs_dict['1']
 
 # Test for importing modules
 def test_execute_request_import_module():
     setup_function()
-    req = Request(originId="2", cells=[CodeRequest(id="2", code="import math")], components={})
+    req = Request(originId="2", cells=[CodeRequest(id="2", code="import math", cellType='code')], components={})
     execute_request(req)
     assert 'math' in cell_outputs_dict['2']
 
 # Test for multiple cells with dependencies
 def test_execute_request_multiple_cells_with_dependencies():
     setup_function()
-    req = Request(originId="3", cells=[CodeRequest(id="3", code="a = 1"),
-                         CodeRequest(id="4", code="b = a + 1")], components={})
+    req = Request(originId="3", cells=[CodeRequest(id="3", code="a = 1", cellType='code'),
+                         CodeRequest(id="4", code="b = a + 1", cellType='code')], components={})
     execute_request(req)
     assert 'a' in cell_outputs_dict['3']
     assert 'b' in cell_outputs_dict['4']
