d63db110c76e3eb62c63b93d0705dd8c7d832106
Carson-Shaar <120226019+Carson-Shaar@users.noreply.github.com>
2023-09-21 17:23:44 -0400

    GHA major update (#33)

--- zt_dev_cli/cli.py
+++ zt_dev_cli/cli.py
@@ -15,6 +15,22 @@ def generate_ts():
     os.chdir('..')
     shutil.rmtree('zt_schema')
 
+def build_frontend():
+    try:
+        shutil.rmtree('zt_backend/dist_dev')
+        shutil.rmtree('zt_backend/dist_app')
+    except Exception as e:
+        print(e)
+    os.chdir('zt_frontend')
+    os.system('yarn install')
+    os.system('yarn run build')
+    os.chdir('..')
+    shutil.copytree('zt_frontend/dist', 'zt_backend/dist_dev')
+    os.chdir('zt_frontend')
+    os.system('yarn run buildapp')
+    os.chdir('..')
+    shutil.copytree('zt_frontend/dist', 'zt_backend/dist_app')
+
 def start_servers(args):
     if args.mode == 'app':
         os.environ['RUN_MODE'] = 'app'
@@ -36,14 +52,16 @@ def zt_cli():
     parser.add_argument("run", nargs='?', default=False, help="Run the project")
     parser.add_argument("mode", choices=["dev", "app"], nargs='?', default="app", help="The mode to run the servers in.")
     parser.add_argument("--frontend-port", type=int, default=5173, help="The port for the frontend server.") # Default to 8080
-    parser.add_argument("--genTS", action=argparse.BooleanOptionalAction, help="Generate TS models without running project")
+    parser.add_argument("--pyd2ts", action=argparse.BooleanOptionalAction, help="Generate TS models without running project")
+    parser.add_argument("--yarnBuild", action=argparse.BooleanOptionalAction, help="Generate TS models without running project")
     args = parser.parse_args()
 
-    if args.genTS:
+    if args.pyd2ts:
         generate_ts()
+    if args.yarnBuild:
+        build_frontend()
     if args.run:
         start_servers(args)
     
-
 if __name__ == "__main__":
     zt_cli()
