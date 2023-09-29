be10dd6f077e30c78dc98e08912511251e7be717
Red-Giuliano <red.giuliano@zero-true.com>
2023-09-29 18:44:02 -0400

    editied code component to remove ts errors part 3

--- zt_frontend/src/App.vue
+++ zt_frontend/src/App.vue
@@ -36,7 +36,7 @@ import { ComponentRequest } from './types/component_request';
 import { DeleteRequest } from './types/delete_request';
 import { CreateRequest, Celltype } from './types/create_request';
 import { Response } from './types/response';
-import { Notebook, CodeCell } from './types/notebook';
+import { Notebook, CodeCell, ZTLayout } from './types/notebook';
 import CodeComponent from '@/components/CodeComponent.vue';
 import MarkdownComponent from '@/components/MarkdownComponent.vue';
 import EditorComponent from '@/components/EditorComponent.vue';
@@ -82,7 +82,7 @@ export default {
       for (const cellResponse of response.cells){
         this.notebook.cells[cellResponse.id].components = cellResponse.components
         this.notebook.cells[cellResponse.id].output = cellResponse.output
-        this.notebook.cells[cellResponse.id].layout = cellResponse.layout
+        this.notebook.cells[cellResponse.id].layout = cellResponse.layout as ZTLayout | undefined;
 
       }
     },
