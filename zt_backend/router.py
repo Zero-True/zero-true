c44c335561e74b86ad56f6f7976504c572e9dc27
Red-Giuliano <red.giuliano@zero-true.com>
2023-09-29 19:13:32 -0400

    added custom serialization for layout so that it will save properly

--- zt_backend/router.py
+++ zt_backend/router.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from zt_backend.models import request, notebook, response
 from zt_backend.runner.execute_code import execute_request
 from zt_backend.models.state import component_values, created_components, context_globals
+from zt_backend.models.components.layout import ZTLayout
 import uuid
 import os
 import toml
+import json
 
 router = APIRouter()
 
@@ -17,7 +19,7 @@ async def runcode(request: request.Request):
     globalStateUpdate(run_request=request)
     response = execute_request(request)
     print(response.cells[0].layout)
-    globalStateUpdate(run_response=response)
+    globalStateUpdate(run_request=request,run_response=response)
     return response
 
 @router.post("/api/component_run")
@@ -48,6 +50,14 @@ def get_notebook():
 def get_notebook():
     with open('notebook.toml', "r") as project_file:
         notebook_data = toml.load(project_file)
+
+    # Convert the JSON strings back to ZTLayout objects
+    for cell_id, cell_data in notebook_data.get('cells', {}).items():
+        layout_str = cell_data.get('layout')
+        if layout_str:
+            layout_dict = json.loads(layout_str)
+            cell_data['layout'] = ZTLayout(**layout_dict)
+
     return notebook.Notebook(**notebook_data)
 
 def globalStateUpdate(newCell: notebook.CodeCell=None, deletedCell: str=None, run_request: request.Request=None, run_response: response.Response=None):
@@ -66,16 +76,20 @@ def globalStateUpdate(newCell: notebook.CodeCell=None, deletedCell: str=None, ru
             zt_notebook.cells[responseCell.id].output = responseCell.output
             zt_notebook.cells[responseCell.id].layout = responseCell.layout
 
+    for cell_id, cell in zt_notebook.cells.items():
+        if cell.layout:
+            cell.layout = json.dumps(cell.layout.dict())
     
     tmp_uuid_file = 'notebook_'+ str(uuid.uuid4())+'.toml'
-
-    try: 
+    
+    try:
         with open(tmp_uuid_file, "w") as project_file:
             toml.dump(zt_notebook.model_dump(), project_file)
         os.replace(tmp_uuid_file,'notebook.toml')
 
     except Exception as e:
-        e
+        print(e)
+            
     try:
         os.remove(tmp_uuid_file)
     except Exception as e:
