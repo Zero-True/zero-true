167557b2acc8843daa47b1e1f8fa90df33465e96
Carson-Shaar <carson.shaar@zero-true.com>
2023-10-05 17:13:57 -0400

    file build

--- zt_frontend/src/types/notebook.d.ts
+++ zt_frontend/src/types/notebook.d.ts
@@ -31,14 +31,11 @@ export type Id1 = string;
 export type VariableName1 = string;
 export type Components1 = ZTComponent[];
 export type Celltype = "code" | "markdown" | "text" | "sql";
-/**
- * User session ID
- */
 export type Userid = string;
 
 export interface Notebook {
   cells: Cells;
-  userId?: Userid;
+  userId: Userid;
   [k: string]: unknown;
 }
 export interface Cells {
