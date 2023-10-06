01d8645c9c69f040c553e344df60dd6ae82e3fcc
Red-Giuliano <red.giuliano@zero-true.com>
2023-10-06 18:28:59 -0400

    adde custom css theme to docs.

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,21 @@
 import os
 import sys
+import shutil
+
 sys.path.insert(0, os.path.abspath('../'))
 
+# Create the _static directory if it doesn't exist
+
+project = 'Zero-True'
 
 extensions = [
     'sphinxcontrib.autodoc_pydantic',
 ]
+
 html_theme = 'furo'
+
+html_title = 'Zero-True documentation'  # Change this line to set your title
+
 html_favicon = '../zt_frontend/public/favicon.ico'
+
+html_css_files = ['custom_theme.css']
