04623a381166d94b478925894e522187663d0053
Carson-Shaar <120226019+Carson-Shaar@users.noreply.github.com>
2023-10-10 18:47:45 -0400

    Card Component and Plotly Changes (#66)
    * Adding Card Component
    
    * Plotylo component update

--- zt_frontend/src/components/LayoutComponent.vue
+++ zt_frontend/src/components/LayoutComponent.vue
@@ -7,9 +7,32 @@
                     <plotly-plot 
                         
                         v-if="comp.component === 'plotly-plot'"
+                        :id="comp.id"
                         :figure="comp.figure"
                         :layout="comp.layout"
                     />
+                    <component 
+                        v-else-if="comp.component==='v-card'" 
+                        :is="comp.component" 
+                        v-bind="componentBind(comp)" 
+                        position="relative"
+                        @runCode="runCode">
+                        <div v-for="c in cardComponents(comp)">
+                            <plotly-plot
+                                v-if="c.component === 'plotly-plot'"
+                                :id="c.id"
+                                :figure="c.figure"
+                                :layout="c.layout"
+                            />
+                            <component
+                                v-else
+                                :is="c.component"
+                                v-bind="componentBind(c)"
+                                v-model="c.value"
+                                @click="clickedButton(c)"
+                                @[c.triggerEvent]="runCode(c.id, c.value)"/>
+                        </div>
+                    </component>
                     <!-- Logic to render other types of components -->
                     <component
                         v-else
@@ -37,9 +60,33 @@
                     <plotly-plot 
                         
                         v-if="comp.component === 'plotly-plot'"
+                        :id="comp.id"
                         :figure="comp.figure"
                         :layout="comp.layout"
                     />
+
+                    <component 
+                        v-else-if="comp.component==='v-card'" 
+                        :is="comp.component" 
+                        v-bind="componentBind(comp)" 
+                        position="relative"
+                        >
+                        <div v-for="c in cardComponents(comp)">
+                            <plotly-plot
+                                v-if="c.component === 'plotly-plot'"
+                                :id="c.id"
+                                :figure="c.figure"
+                                :layout="c.layout"
+                            />
+                            <component
+                                v-else
+                                :is="c.component"
+                                v-bind="componentBind(c)"
+                                v-model="c.value"
+                                @click="clickedButton(c)"
+                                @[c.triggerEvent]="runCode(c.id, c.value)"/>
+                        </div>
+                    </component>
 
                     <!-- Logic to render other types of components -->
                     <component
@@ -65,12 +112,13 @@
 
 <script lang="ts">
 import type { PropType } from 'vue'
-import { VSlider, VTextField, VTextarea, VRangeSlider, VSelect, VCombobox, VBtn, VImg, VAutocomplete } from 'vuetify/lib/components/index.mjs';
+import { VSlider, VTextField, VTextarea, VRangeSlider, VSelect, VCombobox, VBtn, VImg, VAutocomplete, VCard } from 'vuetify/lib/components/index.mjs';
 import { VDataTable } from "vuetify/labs/VDataTable";
 import { Column, ZTComponent, Row } from '@/types/notebook';
 import PlotlyPlot from '@/components/PlotlyComponent.vue';
 
 export default {
+    emits: ["runCode"],
     components: {
       'v-slider': VSlider,
       'v-text-field': VTextField,
@@ -83,6 +131,7 @@ export default {
       'v-img': VImg,
       'v-data-table': VDataTable,
       'v-autocomplete': VAutocomplete,
+      'v-card': VCard,
       'plotly-plot': PlotlyPlot,
     },
     props: {
@@ -101,6 +150,14 @@ export default {
         findComponentById(id: string) {
             const component = this.components.find(comp => comp.id === id);
             return component ? [component] : [];
+        },
+        
+        cardComponents(card: any) {
+            const cardComponents: any[] = []
+            for(const id in card.cardChildren){
+                cardComponents.push.apply(cardComponents,this.findComponentById(card.cardChildren[id]))
+            }
+            return cardComponents
         },
         runCode(componentId: string, componentValue: any) {
             this.$emit('runCode', true, componentId, componentValue);
