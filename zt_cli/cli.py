9d4b4093392f87497e6f87a67d8de6ce49ae2eb9
Red-Giuliano <red.giuliano@zero-true.com>
2023-10-16 12:36:10 -0400

    added typer in place of argparse for the zero-true CLI. Set logging to error and printing localhost instead of 0.0.0.0

--- zt_cli/cli.py
+++ zt_cli/cli.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
 
-import argparse
 import os
+import typer
+from typing_extensions import Annotated
+import logging
 import uvicorn
+from typing import Optional
 
-def start_servers():
-    parser = argparse.ArgumentParser(description="Start the frontend and backend servers.")
-    parser.add_argument("mode", choices=["notebook", "app"], help="The mode to run the servers in.")  # Updated choices
-    args = parser.parse_args()
+app = typer.Typer()
 
-    if args.mode == 'app':
+def print_ascii_logo():
+    ascii_logo="""
+
+_______________________________ ________    _____________________ ____ ______________
+\____    /\_   _____/\______   \\_____  \   \__    ___/\______   \    |   \_   _____/
+  /     /  |    __)_  |       _/ /   |   \    |    |    |       _/    |   /|    __)_ 
+ /     /_  |        \ |    |   \/    |    \   |    |    |    |   \    |  / |         |
+/_______ \/_______  / |____|_  /\_______  /   |____|    |____|_  /______/ /_______  /
+        \/        \/         \/         \/                     \/                 \/ 
+
+    """
+    print(ascii_logo)
+
+@app.command()
+def start(mode: Annotated[Optional[str], typer.Argument(help="The mode to run zero-true in, can be one of 'notebook' and 'app'")],
+          host: Annotated[Optional[str], typer.Argument(help="Host address to bind to.")]="0.0.0.0",
+          port: Annotated[Optional[str],typer.Argument(help="Port number to bind to.")]=""):
+    """
+    Start the Zero-True application.
+    """
+
+    print_ascii_logo()
+    if mode == 'app':
         os.environ['RUN_MODE'] = 'app'
-        uvicorn.run('zt_backend.main:app', host="0.0.0.0", port=2613)
-    elif args.mode=='notebook':
+        port = port if port else 2613
+    elif mode == 'notebook':
         os.environ['RUN_MODE'] = 'dev'
-        uvicorn.run('zt_backend.main:app', host="0.0.0.0", port=1326)
+        port = port if port else 1326
     else:
-        raise Exception('You must specify either noteboook or app to run Zero-True')
+        typer.echo("Invalid mode. Choose either 'notebook' or 'app'.")
+        raise typer.Exit(code=1)
+    
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    
+    if host == "0.0.0.0":
+        host = 'localhost'
+    logger.info(f'Starting Zero-True in {mode} mode on http://{host}:{port}')
+
+    uvicorn.run('zt_backend.main:app', host=host, port=port, log_level='error')
 
 if __name__ == "__main__":
-    start_servers()
+    app()
