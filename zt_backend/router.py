886bbe3033e14f0361ef166f893ebe934ba69c93
Red-Giuliano <red.giuliano@zero-true.com>
2023-11-14 16:31:53 -0500

    empty string caused issues, removing the line has fixed it

--- zt_backend/router.py
+++ zt_backend/router.py
@@ -191,7 +191,7 @@ def get_notebook(id=''):
             'notebookId' : toml_data['notebookId'],
             'userId' : '',
             'cells': {
-                cell_id: notebook.CodeCell(id=cell_id, **cell_data,output="",variable_name='')
+                cell_id: notebook.CodeCell(id=cell_id, **cell_data,output="")
                 for cell_id, cell_data in toml_data['cells'].items()
             }
         }
