da6e1a1aebcb3d5a85d4866d865de1b98bde83bd
Carson-Shaar <carson.shaar@zero-true.com>
2023-12-12 11:56:41 -0500

    another

--- zt_backend/tests/test_e2e.py
+++ zt_backend/tests/test_e2e.py
@@ -44,9 +44,9 @@ def driver():
     options = Options()
     options.add_argument("--no-sandbox") # Bypass OS security model
     options.add_argument("--headless")
-    options.add_argument("--disable-gpu") # applicable to windows os only
+    #options.add_argument("--disable-gpu") # applicable to windows os only
     options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
-    options.add_argument("--remote-debugging-port=9222")
+    #options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
