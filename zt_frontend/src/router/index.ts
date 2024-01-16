afbaec4c2950080e9a48eb0813ee492d24a8a778
Carson-Shaar <carson.shaar@zero-true.com>
2024-01-16 16:47:48 -0500

    Fix for routing to proper url for deployed notebooks

--- zt_frontend/src/router/index.ts
+++ zt_frontend/src/router/index.ts
@@ -1,8 +1,15 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import axios from 'axios'
+
+const getBaseUrl = async () => {
+  const response = await axios.get(import.meta.env.VITE_BACKEND_URL + "base_path");
+  console.log(response.data)
+  return response.data
+}
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(await getBaseUrl()),
 })
 
 export default router
