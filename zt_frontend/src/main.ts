ff7d4b93fb10e360181a9a0eef166fe58291eb29
Carson-Shaar <carson.shaar@zero-true.com>
2024-01-17 13:49:03 -0500

    different spot

--- zt_frontend/src/main.ts
+++ zt_frontend/src/main.ts
@@ -4,7 +4,7 @@ import "@mdi/font/css/materialdesignicons.css";
 import { registerPlugins } from "@/plugins";
 
 const app = createApp(App);
-registerPlugins(app);
+await registerPlugins(app);
 
 app.config.globalProperties.$devMode = "dev" === import.meta.env.VITE_APP_MODE;
 
--- zt_frontend/src/plugins/index.ts
+++ zt_frontend/src/plugins/index.ts
@@ -7,11 +7,20 @@
 // Plugins
 import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
-import router from '../router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import axios from 'axios'
 // Types
 import type { App } from "vue";
 
-export function registerPlugins(app: App) {
+export async function registerPlugins(app: App) {
+  const response = await axios.get(import.meta.env.VITE_BACKEND_URL + "base_path");
+  const baseUrl = response.data;
+
+  // Create the router with the fetched base URL
+  const router = createRouter({
+    history: createWebHistory(baseUrl),
+    // ... (other router options like routes)
+  });
   loadFonts();
   app 
   .use(vuetify)
