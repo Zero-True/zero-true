09a009c890cd8b3b9a1cbce52c255ae18c7b9462
Carson-Shaar <carson.shaar@zero-true.com>
2024-02-15 19:01:59 -0500

    tests

--- zt_backend/tests/test_execute_code.py
+++ zt_backend/tests/test_execute_code.py
@@ -17,7 +17,7 @@ def setup_function():
 # Test for simple variable assignment
 def test_execute_request_simple_variable():
     notebook_state = setup_function()
-    req = Request(originId="0", cells=[CodeRequest(id="0", code="a = 1", variable_name="", cellType='code')], components={})
+    req = Request(originId="0", cells=[CodeRequest(id="0", code="a = 1", variable_name="", nonReactive=False, cellType='code')], components={})
     execute_thread = threading.Thread(target=execute_request, args=(req, notebook_state))
     execute_thread.start()
     execute_thread.join()
@@ -27,7 +27,7 @@ def test_execute_request_simple_variable():
 # Test for function definition
 def test_execute_request_function_definition():
     notebook_state = setup_function()
-    req = Request(originId="1", cells=[CodeRequest(id="1", code="def add(x, y): return x + y", variable_name="", cellType='code')], components={})
+    req = Request(originId="1", cells=[CodeRequest(id="1", code="def add(x, y): return x + y", variable_name="", nonReactive=False, cellType='code')], components={})
     execute_thread = threading.Thread(target=execute_request, args=(req, notebook_state))
     execute_thread.start()
     execute_thread.join()
@@ -36,7 +36,7 @@ def test_execute_request_function_definition():
 # Test for importing modules
 def test_execute_request_import_module():
     notebook_state = setup_function()
-    req = Request(originId="2", cells=[CodeRequest(id="2", code="import math", variable_name="", cellType='code')], components={})
+    req = Request(originId="2", cells=[CodeRequest(id="2", code="import math", variable_name="", nonReactive=False, cellType='code')], components={})
     execute_thread = threading.Thread(target=execute_request, args=(req, notebook_state))
     execute_thread.start()
     execute_thread.join()
@@ -45,8 +45,8 @@ def test_execute_request_import_module():
 # Test for multiple cells with dependencies
 def test_execute_request_multiple_cells_with_dependencies():
     notebook_state = setup_function()
-    req = Request(originId="3", cells=[CodeRequest(id="3", code="a = 1", variable_name="", cellType='code'),
-                        CodeRequest(id="4", code="b = a + 1", variable_name="", cellType='code')], components={})
+    req = Request(originId="3", cells=[CodeRequest(id="3", code="a = 1", variable_name="", nonReactive=False, cellType='code'),
+                        CodeRequest(id="4", code="b = a + 1", variable_name="", nonReactive=False, cellType='code')], components={})
     execute_thread = threading.Thread(target=execute_request, args=(req, notebook_state))
     execute_thread.start()
     execute_thread.join()
