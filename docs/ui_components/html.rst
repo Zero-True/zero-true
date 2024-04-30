25d9061143ac3d6ace044f4bbb525158547786bd
Carson-Shaar <carson.shaar@zero-true.com>
2024-04-30 14:15:33 -0400

    docs and remove requirements

---
+++ docs/ui_components/html.rst
@@ -0,0 +1,7 @@
+HTML
+====
+
+.. autopydantic_model:: zero_true.HTML
+    :model-show-field-summary: false
+    :model-show-validator-summary: false
+    :model-show-validator-members: false
--- zt_backend/models/components/html.py
+++ zt_backend/models/components/html.py
@@ -4,4 +4,4 @@ from zt_backend.models.components.zt_component import ZTComponent
 class HTML(ZTComponent):
     """This is a component is used to render arbitrary HTML"""
     component: str = Field("zt-html", description="Vue component name")
-    v_html: str = Field('', description="HTML content of the div")
+    v_html: str = Field('', description="HTML content of the component")
