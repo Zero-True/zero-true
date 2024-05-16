f848f93bcbeef93458392a7b4cfa7f03f7374af2
Carson-Shaar <120226019+Carson-Shaar@users.noreply.github.com>
2024-05-16 14:29:55 -0400

    fix: Dependency error handling and ZT dependency not in frontend (#256)

--- zt_backend/utils/dependencies.py
+++ zt_backend/utils/dependencies.py
@@ -1,3 +1,4 @@
+from ast import parse
 from fastapi import WebSocket
 from zt_backend.models.api import request
 from zt_backend.models import notebook
@@ -8,18 +9,26 @@ import pkg_resources
 
 logger = logging.getLogger("__name__")
 
+
 def parse_dependencies():
     dependencies = []
-    with open('requirements.txt', 'r', encoding='utf-8') as file:
+    with open("requirements.txt", "r", encoding="utf-8") as file:
         for line in file:
             if line.strip():
                 try:
-                    package, version = line.strip().split('==')
-                    dependencies.append(notebook.Dependency(package=package, version=version))
+                    package, version = line.strip().split("==")
+                    if package != "zero-true":
+                        dependencies.append(
+                            notebook.Dependency(package=package, version=version)
+                        )
                 except:
                     package = line.strip()
-                    dependencies.append(notebook.Dependency(package=package, version=""))
+                    if package != "zero-true":
+                        dependencies.append(
+                            notebook.Dependency(package=package, version="")
+                        )
     return notebook.Dependencies(dependencies=dependencies)
+
 
 def check_env(dependencies: notebook.Dependencies):
     for dependency in dependencies.dependencies:
@@ -27,9 +36,11 @@ def check_env(dependencies: notebook.Dependencies):
             return False
     return True
 
+
 def write_dependencies(dependencies: notebook.Dependencies):
-    with open('requirements.txt', 'w', encoding='utf-8') as file:
+    with open("requirements.txt", "w", encoding="utf-8") as file:
         file.seek(0)
+        file.write(f"zero-true=={pkg_resources.get_distribution('zero-true').version}\n")
         for dependency in dependencies.dependencies:
             if dependency.package:
                 if dependency.version:
@@ -38,30 +49,39 @@ def write_dependencies(dependencies: notebook.Dependencies):
                     file.write(f"{dependency.package}\n")
         file.truncate()
 
+
 def check_installed(package, version):
     try:
         dist = pkg_resources.get_distribution(package)
         if version:
             return dist.version == version
         return True
-    except pkg_resources.DistributionNotFound:
+    except:
         return False
 
-async def dependency_update(dependency_request: request.DependencyRequest, websocket: WebSocket):
+
+async def dependency_update(
+    dependency_request: request.DependencyRequest, websocket: WebSocket
+):
     try:
         for dependency in dependency_request.dependencies.dependencies:
             if not check_installed(dependency.package, dependency.version):
-                command=['pip', 'install']
+                command = ["pip", "install"]
                 if dependency.version:
                     command.append(f"{dependency.package}=={dependency.version}")
                 else:
-                    command.append( dependency.package)
-                with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True) as process:
+                    command.append(dependency.package)
+                with subprocess.Popen(
+                    command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+                ) as process:
                     for line in process.stdout:
                         await websocket.send_json({"output": line})
                     process.stdout.close()
+                    return_code = process.wait()
+                    if return_code != 0:
+                        return parse_dependencies()
         write_dependencies(dependency_request.dependencies)
-        subprocess.run(['lock', 'requirements.txt'])
+        subprocess.run(["lock", "requirements.txt"])
         return parse_dependencies()
     except Exception as e:
         logger.error("Error updating dependencies: %s", traceback.format_exc())
