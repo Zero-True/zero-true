b55898b8b29995738c7a85dec16ff5ace8c19f3e
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-05-24 22:02:33 +1000

    WIP

--- zt_frontend/src/components/comments/Comments.vue
+++ zt_frontend/src/components/comments/Comments.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card title="Comments" class="card">
-    <div class="content pa-4">
+    <div class="content ma-4">
       <v-btn-toggle
         v-model="resolvedMode" 
         :border="false"
@@ -12,21 +12,38 @@
         <v-btn :value="true">Resolved</v-btn>
       </v-btn-toggle>
       <div class="mt-6">
-        <CommentsByCellType />
+        <CommentsPerCell />
       </div> 
+
+      <v-btn
+        :prepend-icon="`ztIcon:${ztAliases.circleAdd}`"
+        variant="outlined"
+        class="w-100 add-comment-btn" 
+      >Add Comment</v-btn>
     </div> 
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { shallowRef } from 'vue'
-import CommentsByCellType from './CommentsByCellType.vue'
+import { ztAliases } from '@/iconsets/ztIcon'
+import CommentsPerCell from './CommentsPerCell.vue'
 
 const resolvedMode = shallowRef(false)
 </script>
 
 <style lang="scss" scoped>
 .card {
-  position: sticky; top: 100px;
+  position: sticky;
+  top: 100px;
+}
+.content {
+  position: relative;
+  height: 100%;
+  min-height: 700px;
+}
+.add-comment-btn {
+  position: absolute;
+  bottom: 0;
 }
 </style>
