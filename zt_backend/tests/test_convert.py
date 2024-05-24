64a7968fd88a0400fc3bd960399c2326808a8f3a
HonkaDonka <vinsonh789@gmail.com>
2024-05-24 15:05:57 -0500

    Test convert

--- zt_backend/tests/test_convert.py
+++ zt_backend/tests/test_convert.py
@@ -1,6 +1,11 @@
-import convert_ztnb_cell
+from zt.backend.zt_cli import convert, create_ztnb_cell
+from zt_backend.models import notebook
+import rtoml
 
 def test_ipynb_to_ztnb():
+
+    with open("./test_file.ipynb.txt", "r", encoding="utf-8") as f: 
+        notebook = json.loads(f.read()) 
 
     output = [] 
 
@@ -15,19 +20,21 @@ def test_ipynb_to_ztnb():
         if (cell['cell_type'] == 'markdown'):
             create_ztnb_cell('"markdown"', cell['source'], output)
 
-    with open('notebook.ztnb', "r") as project_file:
-        toml_data = rtoml.loads(project_file.read().replace('\\','\\\\'))
+    with open("notebook.ztnb", 'w') as f:
+        for item in output:
+            f.write(item + '\n') 
+        toml_data = rtoml.loads(f.read().replace('\\','\\\\'))
 
     notebook_data = {
-            'notebookId' : toml_data['notebookId'],
-            'notebookName' : toml_data.get('notebookName', 'Zero True'),
-            'userId' : '',
-            'cells': {
-                cell_id: notebook.CodeCell(id=cell_id, **cell_data, output="")
-                for cell_id, cell_data in toml_data['cells'].items()
-            }
+        'notebookId' : toml_data['notebookId'],
+        'notebookName' : toml_data.get('notebookName', 'Zero True'),
+        'userId' : '',
+        'cells': {
+            cell_id: notebook.CodeCell(id=cell_id, **cell_data, output="")
+            for cell_id, cell_data in toml_data['cells'].items()
         }
-        
+    }
+
     notebook_state.zt_notebook = notebook.Notebook(**notebook_data)
 
-    
+    assert notebook_state.zt_notebook.notebookId == toml_data['notebookId']
