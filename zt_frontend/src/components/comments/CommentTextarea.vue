3eec5e8e7d27d5ea23400938ee693984f9527254
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-05-25 20:16:21 +1000

    WIP

---
+++ zt_frontend/src/components/comments/CommentTextarea.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-textarea
+    :model-value="modelValue" 
+    @update:model-value="val => emit('update:modelValue', val)" 
+    variant="outlined" 
+  ></v-textarea>
+  <div class="d-flex justify-end">
+    <v-btn
+      variant="text"
+      @click="emit('close')"
+    >Cancel</v-btn>
+    <v-btn 
+      color="primary" 
+      class="ml-2"
+      :loading="isSaving"
+      :disabled="!modelValue || isSaving" 
+      @click="emit('submit')"   
+    >Submit</v-btn>
+  </div> 
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  isSaving: Boolean,
+  modelValue: String
+})
+
+const emit = defineEmits(['close', 'submit', 'update:modelValue'])
+</script>
+
+<style lang="scss" scoped>
+</style>
