7f473d8572aed74ca2836ee56449454d7c5c40fd
Harshita Poojary <dmpoojary6458@gmail.com>
2024-08-05 18:38:02 -0700

    fix: added color for None in card

--- zt_backend/models/components/card.py
+++ zt_backend/models/components/card.py
@@ -8,7 +8,7 @@ class Card(ZTComponent):
     
     component: str = Field("v-card", description="Vue component name")
     childComponents: List[str] = Field([], description="List of child component ids to be placed within the card")
-    color: Optional[str] = Field(None, description="Background color of the card")
+    color: Optional[str] = Field('bluegrey-darken-3', description="Background color of the card")
     elevation: Optional[int] = Field(0, ge=0, le=24, description="Elevation level of the card. Must be between 0 and 24")
     density: Optional[str] = Field('default', enum=['default','comfortable','compact'], description="Density of the component")
     width: Optional[Union[int,str]] = Field('w-auto', description="Width of the card")
