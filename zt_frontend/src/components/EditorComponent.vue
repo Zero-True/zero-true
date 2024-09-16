74b80d3b946e7c6272a1f361f9975aa61d021db4
Carson-Shaar <120226019+Carson-Shaar@users.noreply.github.com>
2024-09-16 15:21:02 -0400

    fix: Extend text editor and file explorer extension (#325)
    * fix(file_directory_management): Add functionality to add/rename/delete folder and file in directory structure
    
    * fix: Extend text editor and file explorer extension
    
    * fix: Bump test dependencies
    
    * fix: Bump copilot express version
    
    ---------
    
    Co-authored-by: priya_kanabar-crest <priya.k@crestinfosystems.com>

--- zt_frontend/src/components/EditorComponent.vue
+++ zt_frontend/src/components/EditorComponent.vue
@@ -1,18 +1,23 @@
 <template>
   <cell
     cell-type="text"
-    :cell-id="cellData.id" 
+    :cell-id="cellData.id"
     :is-dev-mode="$devMode && !isAppRoute && !isMobile"
     :hide-cell="(cellData.hideCell as boolean)"
     :cell-name="(cellData.cellName as string)"
     @save="saveCell"
     @delete="deleteCell"
-    @addCell="e => createCell(e)"
+    @addCell="(e) => createCell(e)"
   >
     <template v-slot:code>
-      <tiny-editor v-if="$devMode && !isAppRoute && !isMobile" v-model="cellData.code" :init="init" @keyUp="saveCell" />
+      <tiny-editor
+        v-if="$devMode && !isAppRoute && !isMobile"
+        v-model="cellData.code"
+        :init="init"
+        @keyUp="saveCell"
+      />
     </template>
-    <template v-slot:outcome v-if="($devMode && isAppRoute) || !$devMode ">
+    <template v-slot:outcome v-if="($devMode && isAppRoute) || !$devMode">
       <tiny-editor v-model="cellData.code" :init="app_init" :disabled="true" />
     </template>
   </cell>
@@ -26,14 +31,20 @@ import "tinymce/themes/silver";
 import "tinymce/icons/default";
 import "tinymce/skins/ui/tinymce-5-dark/skin.css";
 import "tinymce/plugins/autoresize";
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/link";
+import "tinymce/plugins/table";
+import "tinymce/plugins/image";
 import Editor from "@tinymce/tinymce-vue";
 import { CodeCell } from "@/types/notebook";
 import { useRoute } from "vue-router";
-import Cell from '@/components/Cell.vue'
+import Cell from "@/components/Cell.vue";
 
 export default {
   components: {
-    "cell": Cell,
+    cell: Cell,
     "tiny-editor": Editor,
   },
   props: {
@@ -43,68 +54,69 @@ export default {
     },
   },
   inheritAttrs: false,
-  emits: ['saveCell', 'deleteCell', 'createCell'],
+  emits: ["saveCell", "deleteCell", "createCell"],
   data() {
-      return {
-        init: {
-          plugins: "autoresize",
-          toolbar:
-            "undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl",
-          branding: false,
-          menubar: false,
-          statusbar: false,
-          skin: false,
-          content_css: false,
-          font_formats: "Roboto=Roboto, sans-serif;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;",
-          content_style:
-            "body { background-color: #1B2F3C; color: #FFFFFF; font-family: 'Roboto', sans-serif; }",
-          autoresize_bottom_margin: 10,
-          min_height: 100,
-        },
-        app_init: {
-          plugins: "autoresize",
+    return {
+      init: {
+        plugins: "autoresize advlist autolink lists link image table",
+        toolbar:
+          "undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample table | ltr rtl",
+        branding: false,
+        menubar: false,
+        statusbar: false,
+        skin: false,
+        content_css: false,
+        font_formats:
+          "Roboto=Roboto, sans-serif;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;",
+        content_style:
+          "body { background-color: #1B2F3C; color: #FFFFFF; font-family: 'Roboto', sans-serif; }",
+        autoresize_bottom_margin: 10,
+        min_height: 100,
+      },
+      app_init: {
+        plugins: "autoresize",
         toolbar: false,
         branding: false,
         menubar: false,
         statusbar: false,
         skin: false,
         content_css: false,
-        font_formats: "Roboto=Roboto, sans-serif;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;",
+        font_formats:
+          "Roboto=Roboto, sans-serif;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;",
         content_style:
           "body { background-color: #0E1B23; color: #FFFFFF; font-family: 'Roboto', sans-serif; }",
         min_height: 0,
         autoresize_bottom_margin: 0,
       },
-        items: [
-          { title: 'Code' },
-          { title: 'SQL' },
-          { title: 'Markdown' },
-          { title: 'Text' },
+      items: [
+        { title: "Code" },
+        { title: "SQL" },
+        { title: "Markdown" },
+        { title: "Text" },
       ],
     };
   },
   computed: {
     isAppRoute() {
-      const route = useRoute()
-      return route.path === '/app'
+      const route = useRoute();
+      return route.path === "/app";
     },
     isMobile() {
-      return this.$vuetify.display.mobile
+      return this.$vuetify.display.mobile;
     },
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     saveCell() {
-      if (!this.$devMode) return
-      this.$emit("saveCell", this.cellData.id, this.cellData.code, '', '');
+      if (!this.$devMode) return;
+      this.$emit("saveCell", this.cellData.id, this.cellData.code, "", "");
     },
     deleteCell() {
       this.$emit("deleteCell", this.cellData.id);
     },
-    createCell(cellType: string){
+    createCell(cellType: string) {
       this.$emit("createCell", this.cellData.id, cellType);
-    }
+    },
   },
 };
 </script>
