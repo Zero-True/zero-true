6464bebb57fa7e2f9f9c69cacb8441c780da7f08
Red Giuliano <red.giuliano@zero-true.com>
2025-01-09 13:00:36 -0500

    optional requirements

--- zt_backend/models/components/html.py
+++ zt_backend/models/components/html.py
@@ -2,7 +2,6 @@ from pydantic import Field
 from typing import Union
 from zt_backend.models.components.zt_component import ZTComponent
 import pandas as pd
-import pygwalker as pyg
 
 class HTML(ZTComponent):
     """This is a component is used to render arbitrary HTML"""
@@ -11,4 +10,12 @@ class HTML(ZTComponent):
 
 def pygwalker(id: str, df: pd.DataFrame, width: Union[int, str] = '100%', height: Union[int, str] = 920):
     """Create a PyGWalker component from a pandas DataFrame"""
+    try:
+        import pandas as pd
+        import pygwalker as pyg
+    except ImportError as e:
+        raise ImportError(
+            "pygwalker or pandas is not installed. Please install with 'pip install pygwalkwer'."
+        ) from e
+        
     return HTML(id=id, v_html=pyg.to_html(df, width=width, height=height))
