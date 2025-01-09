1c7de58695e12c54da9ffc3fae8ce39a8155d9d9
Red Giuliano <red.giuliano@zero-true.com>
2025-01-09 15:52:32 -0500

    creating a class instead of using unnecessary dependency.

--- zt_backend/config.py
+++ zt_backend/config.py
@@ -1,17 +1,17 @@
-from pydantic_settings import BaseSettings
 from functools import lru_cache
 
-class Settings(BaseSettings):
-    run_mode: str = "app"
-    project_name: str = ""
-    user_name: str = ""
-    team_name: str = ""
-    ws_url: str = ""
-    local_url: str = ""
-    publish_url: str = "https://bxmm0wp9zk.execute-api.us-east-2.amazonaws.com/default/"
-    comments_enabled: bool = False
-    show_create_button: bool = False
-    zt_path: str = ""
+class Settings:
+    def __init__(self):
+        self.run_mode = "app"
+        self.project_name = ""
+        self.user_name = ""
+        self.team_name = ""
+        self.ws_url = ""
+        self.local_url = ""
+        self.publish_url = "https://bxmm0wp9zk.execute-api.us-east-2.amazonaws.com/default/"
+        self.comments_enabled = False
+        self.show_create_button = False
+        self.zt_path = ""
 
 @lru_cache()
 def get_settings():
